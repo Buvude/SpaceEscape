f1f8ee20ffd8aeee4f80c80e5a5b01b2f0739622
Buvude <90856761+Buvude@users.noreply.github.com>
2022-11-05 11:51:28 -0400

    THE PULLING WORKS

--- Space Escape/Assets/CustomScripts/Hands/HandController.cs
+++ Space Escape/Assets/CustomScripts/Hands/HandController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
+
 public class HandController : MonoBehaviour
 {
     public float radius;
     public LayerMask CollisionLayers;
     public InputActionManager inputManager;
+    public GameObject VRPlayerOrigin;
+    private Vector3 surfactGripTarget;
+    private bool grabbing = false;
+    public Transform controllerTarget;
+    public Vector3 followOffset;
+    public float gripMoveLerpRate;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +30,30 @@ public class HandController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 offsetTargetPos = controllerTarget.position + controllerTarget.TransformVector(followOffset);                              //Get actual target position to seek (ALWAYS home toward this, not controllerTarget)
+        if (grabbing)
+        {
+            Vector3 currentOriginPos = VRPlayerOrigin.transform.position;
+            Vector3 targetOriginPos = currentOriginPos;
+            targetOriginPos += surfactGripTarget - offsetTargetPos;
+
+            targetOriginPos = Vector3.Lerp(currentOriginPos, targetOriginPos, gripMoveLerpRate * Time.deltaTime);
+            
+            
+            //Cleanup
+            /*lastOriginVelocity = targetOriginPos - currentOriginPos;*/   //Record velocity
+            VRPlayerOrigin.transform.position = targetOriginPos; //Apply positional change
+
+        }
+
         
     }
 
     public void OnInput(InputAction.CallbackContext context)
     {
         if (context.action.name == "Grab") OnGrab(context);
+
+        
     }
     public void OnGrab(InputAction.CallbackContext context)
     {
@@ -37,14 +62,14 @@ public class HandController : MonoBehaviour
             if (Physics.CheckSphere(transform.position, radius, CollisionLayers))
             {
                 Debug.Log("Grabbed");
-                StartCoroutine("LeftHandLocomotion");
+                /*StartCoroutine("LeftHandLocomotion");*/
+                grabbing = true;
             }
         }
     }
 
-    IEnumerator LeftHandLocomotion()
+    /*IEnumerator LeftHandLocomotion()
     {
-
         yield return 0;
-    }
+    }*/
 }
