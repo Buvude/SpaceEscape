6fe5408baf5021a31e3a1312035c72cb1b6b1c23
Buvude <90856761+Buvude@users.noreply.github.com>
2022-11-21 14:37:45 -0500

    good stuff added hopefully

--- Space Escape/Assets/CustomScripts/Hands/HandController.cs
+++ Space Escape/Assets/CustomScripts/Hands/HandController.cs
@@ -8,7 +8,7 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs;
 public class HandController : MonoBehaviour
 {
     public float radius;
-    public LayerMask CollisionLayers;
+    public LayerMask CollisionLayersLocomotion;
     public InputActionManager inputManager;
     public GameObject VRPlayerOrigin;
     private Vector3 surfactGripTarget;
@@ -16,13 +16,14 @@ public class HandController : MonoBehaviour
     public Transform controllerTarget;
     public Vector3 followOffset;
     public float gripMoveLerpRate;
+    public HandDismissal hD; 
 
     // Start is called before the first frame update
     void Start()
     {
         /*surfactGripTarget *= -1;*/
-        inputManager.actionAssets[0].FindActionMap("XRI LeftHand Interaction").actionTriggered += OnInput;
-        inputManager.actionAssets[0].FindActionMap("XRI RightHand Interaction").actionTriggered += OnInput; //This makes it so both left and right hand are calling the same function... may need to change later
+        inputManager.actionAssets[0].FindActionMap("XRI LeftHand Interaction").actionTriggered += OnInputR;
+        inputManager.actionAssets[0].FindActionMap("XRI RightHand Interaction").actionTriggered += OnInputL; //This makes it so both left and right hand are calling the same function... may need to change later [fixxed]
        /* for(int i = 0; i<7; i++)
         {
             Debug.Log(inputManager.actionAssets[0].actionMaps[i].ToString()); to figure out how to activate the right hand
@@ -30,8 +31,8 @@ public class HandController : MonoBehaviour
     }
     private void OnDisable()
     {
-        inputManager.actionAssets[0].actionMaps[2].actionTriggered -= OnInput;
-        inputManager.actionAssets[0].actionMaps[5].actionTriggered -= OnInput;
+        inputManager.actionAssets[0].actionMaps[2].actionTriggered -= OnInputR;
+        inputManager.actionAssets[0].actionMaps[5].actionTriggered -= OnInputL;
     }
 
     // Update is called once per frame
@@ -55,23 +56,38 @@ public class HandController : MonoBehaviour
 
         
     }
-
-    public void OnInput(InputAction.CallbackContext context)
+    public void SwitchHands()
     {
-        if (context.action.name == "Grab") OnGrab(context);
+        grabbing = false;
+    }
+    public void OnInputL(InputAction.CallbackContext context)
+    {
+        if (context.action.name == "Grab") OnGrab(context, false);
+    }
+    public void OnInputR(InputAction.CallbackContext context)
+    {
+        if (context.action.name == "Grab") OnGrab(context, true);
 
 
     }
-    public void OnGrab(InputAction.CallbackContext context)
+    public void OnGrab(InputAction.CallbackContext context, bool Right)
     {
         if (context.performed)
-        {
-            if (Physics.CheckSphere(transform.position, radius, CollisionLayers))
+        { 
+          if (Physics.CheckSphere(transform.position, radius, CollisionLayersLocomotion))
             {
                 Debug.Log("Grabbed");
                 /*StartCoroutine("LeftHandLocomotion");*/
                 grabbing = true;
                 surfactGripTarget = transform.position;
+                if (Right)
+                {
+                    hD.newRightGrab();
+                }
+                if (!Right)
+                {
+                    hD.newLeftGrab();
+                }
             }
         }
         if (context.canceled)
